2a6f309f51f545cc2e1f127a69a773bface8b4ef
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-05-12 22:16:00 +0600

    service area added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import AboutSection from '../AboutSection/AboutSection';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <AboutSection></AboutSection>
+            <Services></Services>
         </div>
     );
 };
---
+++ src/pages/Home/Services/Services.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+
+    const [services, setServices] = useState([])
+
+    useEffect( () => {
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    }, [])
+
+    return (
+        <div className='mt-4'>
+            <div className='text-center'>
+                <h1 className="text-3xl text-orange-600">Services</h1>
+                <h1 className='text-5xl font-bold'>Our Service Area</h1>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don't look even slightly believable. </p>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                {
+                    services.map( service => <ServiceCard
+                    key={service._id}
+                    service={service}
+                    ></ServiceCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
