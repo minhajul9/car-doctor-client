a5309553fa335703d3f91fec7246c1a0401941bf
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-05-14 01:05:20 +0600

    data sending to server but database not yet

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import Signup from "../pages/Login/Signup";
+import CheckOut from "../pages/CheckOut/CheckOut";
 
 const router = createBrowserRouter([
     {
@@ -20,6 +21,11 @@ const router = createBrowserRouter([
             {
                 path: 'signup',
                 element: <Signup></Signup>
+            },
+            {
+                path: 'checkout/:id',
+                element: <CheckOut></CheckOut>,
+                loader: ({params}) => fetch(`http://localhost:5000/checkout/${params.id}`)
             },
             
         ]
--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -5,10 +5,10 @@ const Services = () => {
 
     const [services, setServices] = useState([])
 
-    useEffect( () => {
-        fetch('services.json')
-        .then(res => res.json())
-        .then(data => setServices(data))
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => setServices(data))
     }, [])
 
     return (
@@ -20,9 +20,9 @@ const Services = () => {
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {
-                    services.map( service => <ServiceCard
-                    key={service._id}
-                    service={service}
+                    services.map(service => <ServiceCard
+                        key={service._id}
+                        service={service}
                     ></ServiceCard>)
                 }
             </div>
